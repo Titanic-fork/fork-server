15d40a813340d65391e0da66c692465642c57fc9
DevJun <guswns1659@gmail.com>
2020-08-15 12:20:52 +0900

    feat : 스프링부트 프로젝트 생성 및 gitignore 추가

---
+++ src/main/java/com/titanic/fork/ForkApplication.java
@@ -0,0 +1,13 @@
+package com.titanic.fork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/titanic/fork/ForkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.titanic.fork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
